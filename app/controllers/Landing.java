1f12fe5fd4f330d73eefa262349b8001c5dafa82
mpa <manfred.pauli@gmail.com>
2012-12-07 21:16:29 +0100

    Add Player page and some crosscutting GUI stuff

--- app/controllers/Landing.java
+++ app/controllers/Landing.java
@@ -2,19 +2,21 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.landing;
 
 /**
  * Landing page controller
+ * 
  * @author mpa
  */
-public class Landing extends Controller{ 
-	
+public class Landing extends Controller {
+
 	/**
 	 * Renders the plain landing page
+	 * 
 	 * @return landing page result
 	 */
-	public static Result land(){
-		return ok(landing.render());
+	public static Result land() {
+		return ok(views.html.landing.render());
 	}
+
 }
